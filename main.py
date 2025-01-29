e6c53066a16ec910f340b854ae9f598d9da23a42
Adriana Marques <venus.carmello@gmail.com>
2025-01-29 18:48:25 -0300

    Restore do main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Jornada de Dados")
+
+#variaveis
+idade = 49
+nome = "Adriana"
+
+#print
+print(idade)
+
+#soma
+nova_idade = idade + 1
+print(nova_idade)
+
+#lista
+lista = [1,2,3,4,5]
+
+#metodos
+lista_invertida = lista.reverse()
